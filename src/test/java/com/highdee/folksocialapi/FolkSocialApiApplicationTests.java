a5cb728c1bb88d253ad806fb393940f6e6419542
ALADESIUN IDOWU ADEDAMOLA <highdee.ai@gmail.com>
2024-12-25 17:27:39 -0500

    initial

---
+++ src/test/java/com/highdee/folksocialapi/FolkSocialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highdee.folksocialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolkSocialApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
