0c2e532a119ce99fd292e2d1e70e7a2bbc7d6f3e
ALADESIUN IDOWU ADEDAMOLA <highdee.ai@gmail.com>
2024-12-25 17:27:39 -0500

    initial

---
+++ src/main/java/com/highdee/folksocialapi/FolkSocialApiApplication.java
@@ -0,0 +1,13 @@
+package com.highdee.folksocialapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolkSocialApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FolkSocialApiApplication.class, args);
+    }
+
+}
